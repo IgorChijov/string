fa1cc6cd6e260e7614f534ea4d78a673426981ba
IgorChijov <igorchijof@gmail.com>
2024-05-26 23:20:45 +0300

    домашка по строкам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        {
+            String firstName = "Ivan ";
+            String middleName = "Ivanovich ";
+            String lastName = "Ivanov ";
+            String fullName = firstName + lastName + middleName;
+            System.out.println("Фио сотрудника - " + fullName);
+        }
+
+        {
+            String fullName = "Ivanov Ivan Ivanovich";
+            System.out.println(fullName.toUpperCase());
+        }
+
+        {
+            String fullName = "Иванов Семён Семёнович";
+            fullName = fullName.replace('ё', 'е');
+            System.out.println("Данные ФИО сотрудника - " + fullName);
+
+        }
     }
 }
